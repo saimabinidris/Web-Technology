68e62588f0448b6663288eab37ea2f49fd264ed8
Saima <saimabinidris@gmail.com>
2026-07-30 14:04:45 +0600

    Test Completed

---
+++ Final Term/Lab1/View/task.php
@@ -0,0 +1,14 @@
+<?php
+$name= "Adnan";
+$a=10;
+$b=20;
+$sum=$a+$b;
+    echo "<h1>Hello,This is my first PHP program!</h1>";
+    echo "<p>Name: $name</p>";
+    echo "<p>Sum: $sum</p>";
+?>
+<html>
+    <body>
+        <h1>Hello!This is my first PHP program!</h1>
+</body>
+</html>
